a4201848e516306e9bbd6df70700b79a8328ad9e
Devesh Chaudhary <devesh.chaudhary.30994@gmail.com>
2021-03-30 17:06:10 +0530

    Add files via upload

---
+++ React/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import  Employee from './components/employee'
+import Assn from './components/assn'
+import FetchApi from './components/fetchApi'
+function App() {
+ 
+ 
+ 
+  return (
+    <div>
+     
+     <FetchApi />
+    </div>
+
+  );
+}
+
+export default App;
